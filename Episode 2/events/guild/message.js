66776c3a6612c48a3cfb048c8da9ec21c11a0328
amp5646@yahoo.com <amp5646@yahoo.com>
2020-06-19 04:56:08 -0500

    Episode 2

--- Episode 2/events/guild/message.js
+++ Episode 2/events/guild/message.js
@@ -9,7 +9,7 @@ module.exports = async (client, message) => {
 
     if (!message.member) message.member = await message.guild.fetchMember(message).catch(err => err);
 
-    const args = message.content.startsWith(prefix) ? message.content.slice(prefix.length).trim().split(/ +/g) : message.content.replace(/[^\s]/, "").time().split(/ +/g);
+    const args = message.content.startsWith(prefix) ? message.content.slice(prefix.length).trim().split(/ +/g) : message.content.replace(/[^\s]*/, "").trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
 
     if (cmd.length === 0) return;
