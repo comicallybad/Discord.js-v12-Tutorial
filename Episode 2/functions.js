6a2a5f6e801140febbd23ad54733655983a0d1fd
amp5646@yahoo.com <amp5646@yahoo.com>
2020-06-19 08:24:08 -0500

    Updated del function

--- Episode 2/functions.js
+++ Episode 2/functions.js
@@ -1,15 +1,15 @@
 module.exports = {
     del: async function (message, timeout) {
-        if (message) {
-            if (message.id) {
+        if (message) { //Fix in case bad message
+            if (message.id) { //Fix cannot read ID 
                 if (message.deletable) {
-                    setTimeout(() => {
-                        if (!message.reactions.cache.get("🛑")) {
-                            message.delete({ timeout: 0 }).catch(err => err);
+                    setTimeout(function () {
+                        if (!message.reactions.cache.get('🛑')) {  //messages can now stop from being deleted
+                            message.delete({ timeout: 0 }).catch(err => err) //This gets rid of the annoying "Unknown Message" error.
                         }
                     }, timeout);
                 }
-            }
-        }
-    }
+            } else return;
+        } else return;
+    },
 }
